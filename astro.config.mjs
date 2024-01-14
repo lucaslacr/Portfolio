7d62864ce241f792b0cdd5c4a3af1b4333552090
Lucas <39408032+lucaslacr@users.noreply.github.com>
2024-01-14 14:27:39 +0100

    Ajout de matomo, correction suite au test light house

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
 import { defineConfig } from 'astro/config';
+import matomo from 'astro-matomo';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://lucaslacroix.com",
+  integrations: [
+    matomo({
+      enabled: import.meta.env.PROD, // Only load in production
+      host: "https://audience.lucaslacroix.com/",
+      setCookieDomain: "*.lucaslacroix.com",
+      trackerUrl: "js/", // defaults to matomo.php
+      srcUrl: "js/", // defaults to matomo.js
+      siteId: 1,
+      heartBeatTimer: 5,
+      disableCookies: true,
+      debug: false,
+    }),
+  ]
+});
